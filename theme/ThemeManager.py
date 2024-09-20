2705ba5edaa849a745c531ec31833c252da302e2
felipemelonunes09 <felipe.melonunes09@gmail.com>
2024-09-19 23:02:44 -0300

    added new styles and classes

--- theme/ThemeManager.py
+++ theme/ThemeManager.py
@@ -9,19 +9,48 @@ class ThemeManager:
     def apply_theme(self, theme_name: str, window: QWidget):
         theme = self.theme.get(theme_name, {})
         stylesheet = f"""
-        #InitialFrame {{
+            #InitialFrame {{
+                background-color: { theme['bg-primary-1'] };
+            }}      
+            #InitialButton QWidget {{
+                background-color: { theme['bg-secondary-1'] };
+                font-weight: bold;
+            }}
+            #AttributesFrame {{
+                background-color: { theme['bg-primary-2'] };
+            }}
+            #EditorFrame {{
                 background-color: { theme['bg-primary-1'] };
             }}
-            
-        #InitialButton QWidget {{
-                background-color: { theme['bg-primary-2'] };
-                font-weight: bold;
-        }}
-        
-        #InitialButton:hover {{
-            QWidget {{ background-color:red; }}
-        }}
-        
+            #EditorFrame QPlainTextEdit {{ background-color: { theme['bg-primary-1'] }; }}
+            #FileBar {{  background-color: { theme["bg-primary-2"] };}}
+            #FileTab {{ 
+                border: 1px solid { theme["bg-secondary-1"] }; 
+                padding-left: 15px;
+                padding-right: 15px;
+            }}
+            #CurrentFileTab {{
+                background-color: { theme['bg-primary-1'] };
+                border-top: 4px solid #334EAC;
+                border-left: 1px solid { theme["bg-secondary-1"] }; 
+                border-right: 1px solide { theme["bg-secondary-1"] };  
+            }}
+            #FileTab QPushButton {{
+                border: none;
+                background-color: transparent;
+            }}
+            #CurrentFileTab QPushButton {{
+                border: none;
+                background-color: transparent;
+            }}
+            #CurrentFileTab QPushButton:pressed {{
+                color: #414747;
+            }}
+            #FileTab QPushButton:pressed {{
+                color: #414747;
+            }}
+            #FileTab:hover {{
+                background-color: { theme["bg-secondary-2"] }; 
+            }}
         """
-        
         window.setStyleSheet(stylesheet)
