dc5984a9d90d8504e9dc6c20d5135e53109541fb
felipemelonunes09 <felipe.melonunes09@gmail.com>
2024-10-01 16:16:58 -0300

    added a new method to build de area after its build for the first time allowing splitting

--- core/frame/area_editor_frame.py
+++ core/frame/area_editor_frame.py
@@ -9,23 +9,29 @@ class AreaNode():
         self.orientation = orientation
     def get(self) -> tuple[list[object], int, int]: 
         return (self.leaves, len(self.leaves), self.orientation)       
-
+    
 class AreaEditorFrame(QFrame):
     
     def __init__(self, parent: QWidget) -> None:
         super().__init__(parent)
         self.__editor_list: list[EditorFrame] = list()
         self.__current_editor: EditorFrame = None
+        self.__area_tree = AreaNode(leaves=[], orientation=0)
+        self.__area_widget: QFrame = None
+        self.__area_layout: QBoxLayout = None
+        self.__layout = QVBoxLayout()
+        self.__layout.setContentsMargins(0,0,0,0)
+        self.__layout.setSpacing(0)
+        self.setLayout(self.__layout)
+        self.build_area()
         
-    def teste(self) -> None:
-        self.__area_tree = AreaNode(leaves=[
-            AreaNode(leaves=[], orientation=0),
-            AreaNode(leaves=[
-                AreaNode(leaves=[], orientation=0),
-                AreaNode(leaves=[], orientation=0)
-            ], orientation=1)    
-        ], orientation=0)
-        self.setLayout(self.__build_area(self.__area_tree, self))
+    def build_area(self) -> None:
+        if self.__area_widget:
+            self.__area_widget.deleteLater()
+        self.__area_widget = QFrame() 
+        self.__area_layout = self.__build_area(self.__area_tree, self.__area_widget)
+        self.__area_widget.setLayout(self.__area_layout)
+        self.__layout.addWidget(self.__area_widget)
         
     def get_current_editor(self) -> EditorFrame:
         return self.__current_editor
@@ -34,6 +40,7 @@ class AreaEditorFrame(QFrame):
         self.__current_editor = editor      
         
     def __create_editor(self) -> EditorFrame:
+        print("creating editor")
         new_editor = EditorFrame(self)
         self.__editor_list.append(new_editor)
         self.set_current_editor(new_editor)
@@ -45,7 +52,8 @@ class AreaEditorFrame(QFrame):
         layout = QVBoxLayout(parent) if orientation == 1 else QHBoxLayout(parent)
         layout.setContentsMargins(0,0,0,0)
         if height == 0:
-            layout.addWidget(self.__create_editor())
+            frame = self.__create_editor()
+            layout.addWidget(frame)
             return layout
         spliter = QSplitter(Qt.Vertical) if orientation == 1 else QSplitter(Qt.Horizontal)
         layout.addWidget(spliter)
