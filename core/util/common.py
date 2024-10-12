e810cc464dda62e768117adbdf3cf894269524a7
felipemelonunes09 <felipe.melonunes09@gmail.com>
2024-10-12 18:26:36 -0300

    add direction enum

--- core/util/common.py
+++ core/util/common.py
@@ -1,5 +1,13 @@
+from enum import Enum
 import math
 
 def distance(pointA: tuple, pointB: tuple) -> int:
     dist = math.sqrt(sum((a - b) ** 2 for a, b in zip(pointA, pointB)))
     return int(dist)
+
+class Direction(Enum):
+    RIGHT = 1
+    LEFT = 2
+    TOP = 3
+    BOTTOM = 4
+    CENTER = 5
