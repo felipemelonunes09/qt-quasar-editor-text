d07e626d2ea22a0219e4723da9248df58dd49146
felipe-melo <felipe.melonunes09@gmail.com>
2024-12-24 13:55:07 -0300

    changed abs path to relatives path

--- core/frame/initial_frame.py
+++ core/frame/initial_frame.py
@@ -12,7 +12,7 @@ class InitialFrame(QFrame):
     
     def __init__(self, parent=None) -> None:
         super().__init__(parent)
-        self.setObjectName("InitialFrame")
+        self.setObjectName("InitialFrame") 
         self.__layout = QVBoxLayout(self)
         self.__btn_layout = QHBoxLayout()
         btn_open_file = InitialButton(img_path=config.icon_path_rocket_lunch, text="Open File", signal=self.open_file)
